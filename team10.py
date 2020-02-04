d1711c4d1bec57f49200f8cdd47dc1fc3779cf6b
LostRanger45 <60195167+LostRanger45@users.noreply.github.com>
2020-02-04 10:18:46 -0800

    Update team10.py

--- team10.py
+++ team10.py
@@ -8,32 +8,25 @@
 ####
 
 team_name = 'gAmErS'
-strategy_name = 'Collude 90% unless betrayed within last 10 rounds.'
+strategy_name = 'Collude 95% unless betrayed within last 5 rounds or have less than -150 points.'
 strategy_description = '''\
-Betray if ever betrayed.
-If I haven't been betrayed yet, I'll betray starting with the 100th round.
+Betray if they betray in last 5 rounds, or if our score is below -150. Other wise our code will be random with us betraying only 5% of the time
 '''
 
-import random
-    
+import random 
+            
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
-
-    if 'b' in their_history[-10:]: # If the other player has betrayed within last 10 rounds, 
-        return 'b'               # Betray.
+    if my_score < -150:
+        return 'b'
     else:
-        if random.random()<0.1: # 10% of the other rounds
-            return 'b'         # Betray
+        if 'b' in their_history[-5:]:
+            return 'b'
         else:
-            return 'c'         # but 90% of the time collude
+            if random.random()<0.05:
+                return 'b'
+            else:
+                'c'
+    
     
     
     
